0b17dd604221bcf45a3fdaf666ed7cb7c4affe15
Tariq <vfx.tariq@gmail.com>
2025-11-28 01:08:18 +0300

    fix: remove duplicate Header component in Game.tsx

--- pages/Game.tsx
+++ pages/Game.tsx
@@ -677,8 +677,6 @@ const Game: React.FC = () => {
 
       <Header onHelpClick={() => setShowHelp(true)} />
 
-      <Header onHelpClick={() => setShowHelp(true)} />
-
       <main className="flex-1 w-full max-w-2xl mx-auto p-4 flex flex-col relative z-10">
 
         {/* Spectator Mode for Setter */}
